9c7e4028e3de69f9dc4bef12c3544f8033206562
gauravkumar <i.gauravdubey@gmail.com>
2025-01-17 01:01:50 +0530

    Save the basic structure

---
+++ app/src/main/java/com/gaurav/stackoverflowapp/MainApplication.kt
@@ -0,0 +1,5 @@
+package com.gaurav.stackoverflowapp
+
+import android.app.Application
+class MainApplication : Application() {
+}
---
+++ app/src/main/java/com/gaurav/stackoverflowapp/question/FavoriteQuestion.kt
@@ -0,0 +1,4 @@
+package com.gaurav.stackoverflowapp.question
+
+
+data class FavoriteQuestion(val id:String, val question:String)
